ed438c8c28028ae0c07fc5df6c2164e3a9977e8f
Bhavani Palla <bhavani.palla@gmail.com>
2022-09-08 22:52:12 +0200

    Added swagger json and validation for users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
 
 //Get all users
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     res.json(await User.find());
   } catch (error) {
@@ -11,44 +11,63 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:userId', async (req, res) => {
-  console.log(req.params.userId)
+router.get("/:userId", async (req, res) => {
+  console.log(req.params.userId);
   try {
-    res.json(await User.find(
-        {
-          name:  req.params.userId
-        }
-      )
-    )
-     
+    res.json(
+      await User.find({
+        name: req.params.userId,
+      })
+    );
   } catch (error) {
     res.json({ message: error });
   }
 });
 
 //post to create user in db
-
-
-router.post("/", async (req, res) => {
-  console.log("Posted");
+router.post("/login", async (req, res) => {
+  console.log("login user");
   const user = new User({
     userName: req.body.userName,
-    firstName:req.body.firstName,
-    lastName:req.body.lastName,
     password: req.body.password,
-    email: req.body.email,
-    
   });
+  console.log(user);
   try {
-    const savedUser = await user.save();
-    res.json(savedUser);
+    res.json(
+      await User.find({
+        $and: [{ userName: user.userName }, { password: user.password }],
+      })
+    );
   } catch (error) {
     res.json({ message: error });
   }
 });
 
+router.post("/", async (req, res) => {
+  console.log("Posted user");
+  const user = new User({
+    userName: req.body.userName,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: req.body.password,
+    email: req.body.email,
+  });
+  try {
+    //find if there is any existing user with same user id
+    const existingUser = await User.find({ userName: user.userName });
 
+    if (existingUser.length !== 0) {
+      console.log("user with id: " + user.userName + " already existing");  
+      res.status(500).json({ message: 'user already exists'})
+      return
+    }
 
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (error) {
+     res.status(500).json({ message: "Something went wrong" });
+  }
+});
 
 /* router.post('/', async (req, res) => {
   console.log("Connected");
@@ -72,7 +91,7 @@ router.post("/", async (req, res) => {
 });
  */
 // Delete post
-router.delete('/:userId', async (req, res) => {
+router.delete("/:userId", async (req, res) => {
   try {
     const deletedUser = await User.deleteOne({ _id: req.params.userId });
     res.json(deletedUser);
@@ -82,7 +101,7 @@ router.delete('/:userId', async (req, res) => {
 });
 
 // Update post
-router.patch('/:userId', async (req, res) => {
+router.patch("/:userId", async (req, res) => {
   try {
     const updatedUser = await User.updateOne(
       { _id: req.params.userId },
@@ -91,8 +110,6 @@ router.patch('/:userId', async (req, res) => {
           name: req.body.name,
           password: req.body.password,
           email: req.body.email,
-          
-          
         },
       }
     );
