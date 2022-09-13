9cbfbe5d9f2ef2a67ec5001b72ab29a80dbbad49
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-09-13 10:16:22 +0200

    end-points changed, nodemon added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/User");
+const User = require('../models/User');
 
 //Get all users
 /**
@@ -12,7 +12,7 @@ const User = require("../models/User");
  *        '200':
  *            description: A successfull response
  */
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     res.json(await User.find());
   } catch (error) {
@@ -20,7 +20,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:userId", async (req, res) => {
+router.get('/:userId', async (req, res) => {
   console.log(req.params.userId);
   try {
     res.json(
@@ -51,7 +51,7 @@ router.get("/:userId", async (req, res) => {
  *            - userName
  *            - password
  *          properties:
- *              userName: 
+ *              userName:
  *                type: string
  *              password:
  *                type: string
@@ -61,25 +61,25 @@ router.get("/:userId", async (req, res) => {
  *        '500':
  *            description: Techinal exception
  */
-router.post("/login", async (req, res) => {
-  console.log("login user");
+router.post('/login', async (req, res) => {
+  console.log('login user');
   const user = new User({
     userName: req.body.userName,
     password: req.body.password,
   });
- // console.log(user);
+  // console.log(user);
   //Check if user with given user name exists else throw error
   try {
-   const loggedInUser =  await User.find({
+    const loggedInUser = await User.find({
       $and: [{ userName: user.userName }, { password: user.password }],
-   });
-    
+    });
+
     if (loggedInUser && loggedInUser.length != 0) {
-      res.json(
-        loggedInUser
-      );
+      res.json(loggedInUser);
     } else {
-      res.status(404).json("User not found for the given user name and password")
+      res
+        .status(404)
+        .json('User not found for the given user name and password');
     }
   } catch (error) {
     res.status(500).json({ message: error });
@@ -106,7 +106,7 @@ router.post("/login", async (req, res) => {
  *            - email
  *            - password
  *          properties:
- *              userName: 
+ *              userName:
  *                type: string
  *              firstName:
  *                type: string
@@ -116,16 +116,16 @@ router.post("/login", async (req, res) => {
  *                type: string
  *              password:
  *                type: string
- *        
- *      
+ *
+ *
  *    responses:
  *        '200':
  *            description: A successfull response
  *        '500':
  *            description: Techinal problem
  */
-router.post("/", async (req, res) => {
-  console.log("Posted user");
+router.post('/signin', async (req, res) => {
+  console.log('Posted user');
   const user = new User({
     userName: req.body.userName,
     firstName: req.body.firstName,
@@ -133,28 +133,23 @@ router.post("/", async (req, res) => {
     password: req.body.password,
     email: req.body.email,
   });
-   try {
+  try {
     //find if there is any existing user with same user id
     const existingUser = await User.find({ userName: user.userName });
 
     if (existingUser.length !== 0) {
-     
-      res
-        .status(400)
-        .json({
-          message: "user with id: " + user.userName + " already exists."
-        });
+      res.status(400).json({
+        message: 'user with id: ' + user.userName + ' already exists.',
+      });
       return;
     }
 
     const savedUser = await user.save();
     res.json(savedUser);
   } catch (error) {
-    res.status(500).json({ message: "Unable to create a user, " + error});
-  } 
-  
+    res.status(500).json({ message: 'Unable to create a user, ' + error });
+  }
 });
-
 
 // Delete post
 //post to create user in db
@@ -177,7 +172,7 @@ router.post("/", async (req, res) => {
  *        '500':
  *            description: Techinal exception
  */
-router.delete("/:userId", async (req, res) => {
+router.delete('delete/:userId', async (req, res) => {
   try {
     const deletedUser = await User.deleteOne({ _id: req.params.userId });
     res.json(deletedUser);
@@ -203,7 +198,7 @@ router.delete("/:userId", async (req, res) => {
  *          required:
  *            - userName
  *          properties:
- *              userName: 
+ *              userName:
  *                type: string
  *              firstName:
  *                type: string
@@ -219,7 +214,7 @@ router.delete("/:userId", async (req, res) => {
  *        '500':
  *            description: Techinal exception
  */
-router.patch("/:userId", async (req, res) => {
+router.patch('/:userId', async (req, res) => {
   try {
     const updatedUser = await User.updateOne(
       { _id: req.params.userId },
