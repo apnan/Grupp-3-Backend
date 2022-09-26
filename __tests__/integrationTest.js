caf9df4df86db864538da3e6bbea63eff149b7b0
Bhavani Palla <bhavani.palla@gmail.com>
2022-09-26 11:44:11 +0200

    integration test is fixed

--- __tests__/integrationTest.js
+++ __tests__/integrationTest.js
@@ -1,59 +1,64 @@
 /**
  * @group integration
  */
-
-const app = require("../app.js");
 const request = require("supertest");
-const { default: mongoose } = require("mongoose");
-const { response } = require("../app.js");
-const server  = require("../server");
+const app = require("../app");
+const mongoose = require("mongoose");
+const { response } = require("../app");
+const PORT = process.env.HOST || 3001;
 
-// const supertest=require('supertest')
-
-//const HOST = process.env.HOST || "http://localhost:3000/";
-
-dbURI = "mongodb://mongo:27017/test";
-
+let server = app;
 let data = {
-  userName: "dummy1",
+  userName: "dummy19",
   firstName: "dummy1",
   lastName: "dummy1",
   email: "dummy1@mail.com",
-  password: "333444",
+  password: "3333333",
 };
 
-describe("testing api", function () {
-  beforeAll(function (){
-    mongoose.connect(
-      "mongodb://mongo:27017/test",
 
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      () => console.log("Connected to Mongo DB")
-    );
+beforeAll((done) => {
+  server = app.listen(PORT, (err) => {
+    if (err) return done(err);
+    done();
   });
+  mongoose
+    .connect("mongodb://localhost:27017/test", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => console.log(err));
+});
 
-  describe("/POST/signin", function () {
-    it("should add new user", function () {
-    
-       request(app)
-        .post("/api/users/signin")
-        .send(data)
-        .set("Accept", "application/json")
-        .expect(200)
-        .expect("Content-Type", "application/json; charset=utf-8")
-        .then(function (err, response) {
-          if (err) return err;
-        });
-    });
-  });
+afterAll(async() => {
+   await new Promise((resolve) => setTimeout(() => resolve(), 500)); 
+  await mongoose.disconnect();
+  await server.close();
+});
 
-  afterAll(async () => {
-    await new Promise((resolve) => setTimeout(() => resolve(), 500));
-
-    mongoose.disconnect(console.log("Done"));
-
-    // await server.close();
-
-    // avoid jest open handle error
+describe("/POST/signin", function () {
+  it("should add new user", async () => {
+    const resp = await request(app)
+      .post("/api/users/signin")
+      .send(data)
+      .set("Accept", "application/json")
+      .expect(201)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then(function (err, response) {
+        if (err) return err;
+      });
+   
   });
 });
+
+ 
+
+
+
+
+
+
+
+
+
+  
