1033bf0d68a55480794edb815d31389e2d902046
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-09-27 14:42:31 +0200

    Huski try

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 app.use(express.json());
-const usersRoute = require("./routes/users");
-app.use("/api/users", usersRoute);
+const usersRoute = require('./routes/users');
+app.use('/api/users', usersRoute);
 
-
-module.exports = app
+module.exports = app;
+/* ffd */
