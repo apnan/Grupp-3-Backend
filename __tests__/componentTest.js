6b1e0ab2737797ef54bd282e2e2191611a95ac91
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-09-29 19:43:07 +0200

    componenttest will pass

--- __tests__/componentTest.js
+++ __tests__/componentTest.js
@@ -1,14 +1,28 @@
 /**
  * @group component
  */
-const calculator = require('../calculator.js');
 
-test('Calculator should add!', () => {
-  expect(calculator.add('1')).toBe(1);
+class dummyTest {
+  constructor(numbers) {
+    this.numbers = numbers;
+  }
+
+  add(numbers) {
+    return numbers
+      .split(',')
+      .map((x) => parseInt(x))
+      .reduce((a, b) => a + b);
+  }
+}
+
+const dummyComponentTest = new dummyTest();
+
+test('dummyComponentTest should add!', () => {
+  expect(dummyComponentTest.add('1')).toBe(1);
 });
 
-test('Calculator should add!', () => {
-  expect(calculator.add('3,3')).toBe(6);
+test('dummyComponentTest should add!', () => {
+  expect(dummyComponentTest.add('3,3')).toBe(6);
 });
 
 /* 
--- __tests__/unittest.js
+++ __tests__/unittest.js
@@ -1,15 +1,25 @@
 /**
  * @group unit
  */
+class dummyTest {
+  constructor(numbers) {
+    this.numbers = numbers;
+  }
 
-const calculator = require('../calculator.js');
+  add(numbers) {
+    return numbers
+      .split(',')
+      .map((x) => parseInt(x))
+      .reduce((a, b) => a + b);
+  }
+}
 
-test('Calculator should add!', () => {
-  expect(calculator.add('1')).toBe(1);
+const dummyComponentTest = new dummyTest();
+
+test('dummyComponentTest should add!', () => {
+  expect(dummyComponentTest.add('1')).toBe(1);
 });
 
-test('Calculator should add!', () => {
-  expect(calculator.add('2,3')).toBe(5);
+test('dummyComponentTest should add!', () => {
+  expect(dummyComponentTest.add('3,3')).toBe(6);
 });
-
-/* ssdlkfjsdlksjdflk */
