0d91ead40427f6add9509bcbd61bac40c4e9771c
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-10-06 11:46:34 +0200

    deleted unnecessary files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 
 const app = express();
 app.use(express.json());
@@ -6,3 +6,4 @@ const usersRoute = require('./routes/users');
 app.use('/api/users', usersRoute);
 
 module.exports = app;
+ */
