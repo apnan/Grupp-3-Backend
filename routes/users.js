4973e49952260fb45ae989817bef8b3739b3ff44
apnan <aishajahan2015@gmail.com>
2022-10-06 15:10:26 -1200

    trying test

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usersController');
-// test1
+// test2
 //Get all users
 /**
  * @swagger
